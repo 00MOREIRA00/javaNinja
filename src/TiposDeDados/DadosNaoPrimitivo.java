d3903d2dd8b6a74d0cdddbb5d9a864aaa4e3ea1e
00MOREIRA00 <robertocorreamoreiraneto@gmail.com>
2026-03-02 20:28:06 -0300

    feat: adicionando arquivos iniciais do curso

---
+++ src/TiposDeDados/DadosNaoPrimitivo.java
@@ -0,0 +1,7 @@
+package TiposDeDados;
+
+public class DadosNaoPrimitivo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,15 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+        * Dados Primitivos - int, double, float, char, boolean, short.
+        * Objetivo: Criar um ninja
+        * */
+
+        int idade = 16;
+        double altura = 1.65;
+        boolean vivoOuMorto = true;
+        long  saldoBancario = 999999999L;
+    }
+}
