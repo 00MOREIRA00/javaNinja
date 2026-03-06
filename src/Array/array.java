6ad20530b520870e09a90503f9e5edbe445922f0
00MOREIRA00 <robertocorreamoreiraneto@gmail.com>
2026-03-06 00:44:00 -0300

    feat: comecando a criacao do primeiro desafio

--- src/Array/array.java
+++ src/Array/array.java
@@ -7,5 +7,18 @@ public class array {
         ninjas[1] = "Sasuke Uchirra";
         ninjas[2] = "Sakura Haruno";
 
+
+        int[] idades = new int[3];
+        idades[0] = 15;
+        idades[1] = 18;
+        idades[2] = 17;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(idades[i]);
+        }
+
     }
 }
+
+
+//Array é uma referencia na memoria. Cada item é um partimento na memoria alocada
---
+++ src/Desafios/SegundoDesafio.java
@@ -0,0 +1,55 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class SegundoDesafio {
+    public static void main(String[] args) {
+
+        System.out.println("Bem vindo ao sistema de cadastro de ninjas. Selecione uma opção para prosseguir: ");
+        System.out.println("        OPÇÕES");
+        System.out.println("|---------------------|");
+        System.out.println("1 - Cadastrar um novo ninja");
+        System.out.println("2 - Consultar ninjas cadastrados");
+        System.out.println("3 Finalizar aplicação ");
+
+        Scanner sc = new Scanner(System.in);
+        int selecao = sc.nextInt();
+        System.out.println(selecao);
+        sc.close();
+
+        switch (selecao) {
+            case 1:
+                System.out.println("Opção 1 selecionanda");
+
+            case 2:
+                System.out.println("Opção 2 selecionanda");
+
+            case 3:
+                System.out.println("Opção 3 selecionada");
+
+            default:
+                System.out.println("Valor não reconhecido adicionando");
+        }
+    }
+}
+
+
+
+
+/*          Descrição
+*
+* Você foi enccarregado pelo Hokage para gerenciar o cadastro de ninjas na Vila da Folha.
+* Sua missão é criar um sistema simples em Java para cadastrar novos ninjas e listar todos os ninjas cadastrados
+*
+*           Tópicos
+* - Array para armazenar os nomes dos ninjas cadastrados
+* - Switch Cases para implementar um menu interativos
+* - Loops para navegar pelas opções do menu e iterar sobre os ninjas cadastrados
+* - Condicionais para controlar a execução das opções do menu e validar o número de ninjas cadastrados
+*
+*           DICAS EXTRAS
+* - Permita o usuário cadastrar o nome de um ninja
+* - Exiba a lista de todos os ninjas cadastrados
+* - Imprima os dados
+* - O usuário pode encerrar o programa a qualquer momento
+* */
